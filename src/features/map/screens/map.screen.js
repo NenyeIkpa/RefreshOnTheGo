59b6baa60ed4ab66c7d104c421b33d29b3691b25
NenyeIkpa <nenyeikpa@gmail.com>
2022-11-10 10:33:13 +0100

    restructured navigation, linked up restaurant with map screen and ice versa.

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,20 +1,64 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { Dimensions } from "react-native";
 import MapView from "react-native-maps";
 import styled from "styled-components/native";
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Search } from "../components/search.component";
-
-const MapWrapper = styled(MapView)`
+import { LocationContext } from "../../../services/location/location.context";
+import { RestaurantsContext } from "../../../services/restaurants/restauraunts.context";
+import { MapCallout } from "../components/map-callout.component";
+const Map = styled(MapView)`
   width: ${Dimensions.get("window").width}px;
   height: ${Dimensions.get("window").height}px;
 `;
 
-export const MapScreen = () => {
+export const MapScreen = ({ navigation }) => {
+  const [latitudeDelta, setLatitudeDelta] = useState(0); //latitude Delta used to set zoom level on map
+  const { location } = useContext(LocationContext);
+  const { restaurants } = useContext(RestaurantsContext);
+  const { lat, lng, viewport } = location;
+  console.log(viewport);
+
+  useEffect(() => {
+    const northEastLat = viewport.northeast.lat;
+    const southWestLat = viewport.southwest.lat;
+    const latDelta = northEastLat - southWestLat;
+    setLatitudeDelta(latDelta);
+  }, [location, viewport]);
+
   return (
     <>
       <Search />
-      <MapWrapper />
+      <Map
+        region={{
+          latitude: lat,
+          longitude: lng,
+          latitudeDelta: latitudeDelta,
+          longitudeDelta: 0.02,
+        }}
+      >
+        {restaurants.map((restaurant) => {
+          return (
+            <MapView.Marker
+              key={restaurant.name}
+              title={restaurant.name}
+              coordinate={{
+                latitude: restaurant.geometry.location.lat,
+                longitude: restaurant.geometry.location.lng,
+              }}
+            >
+              <MapView.Callout
+                onPress={() =>
+                  navigation.navigate("RestaurantDetailScreen", {
+                    restaurant: restaurant,
+                  })
+                }
+              >
+                <MapCallout restaurant={restaurant} />
+              </MapView.Callout>
+            </MapView.Marker>
+          );
+        })}
+      </Map>
     </>
   );
 };
--- src/infrastructure/navigation/app.navigator.js
+++ src/infrastructure/navigation/app.navigator.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 import { Ionicons } from "@expo/vector-icons";
 import { SafeArea } from "../../components/utility/safe-area.component";
-import { RestaurantsNavigator } from "./restaurant.navigator";
+import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 import { MapScreen } from "../../features/map/screens/map.screen";
+import { RestaurantDetailScreen } from "../../features/restaurants/screens/restaurant-details.screen";
 
 function SettingsScreen() {
   return (
@@ -16,6 +21,8 @@ function SettingsScreen() {
 }
 
 const Tab = createBottomTabNavigator();
+
+const Stack = createStackNavigator();
 
 const screenOptions = ({ route }) => ({
   tabBarIcon: ({ color, size }) => {
@@ -37,14 +44,33 @@ const screenOptions = ({ route }) => ({
   tabBarInactiveTintColor: "gray",
 });
 
+function Root() {
+  return (
+    <Tab.Navigator screenOptions={(screenOptions, { headerShown: false })}>
+      <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
+
 export const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={(screenOptions, { headerShown: false })}>
-        <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-        <Tab.Screen name="Map" component={MapScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator
+        screenOptions={{ ...TransitionPresets.ModalPresentationIOS }}
+      >
+        <Stack.Screen
+          name="Root"
+          component={Root}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="RestaurantDetailScreen"
+          component={RestaurantDetailScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
